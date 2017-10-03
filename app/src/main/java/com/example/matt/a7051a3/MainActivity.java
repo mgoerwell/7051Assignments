8130e7c6c3ab2e35b65eee7c20ea147efa1be567
Matt Goerwell <Superninja246@hotmail.com>
2017-10-03 12:54:44 -0700

    Adding Search activity

--- app/src/main/java/com/example/matt/a7051a3/MainActivity.java
+++ app/src/main/java/com/example/matt/a7051a3/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.matt.a7051a3;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void Search() {
+        Intent intent = new Intent(this, SearchActivity.class);
     }
 }
---
+++ app/src/main/java/com/example/matt/a7051a3/SearchActivity.java
@@ -0,0 +1,18 @@
+package com.example.matt.a7051a3;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Matt on 03/10/2017.
+ */
+
+public class SearchActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
+}
+
+
