a19138e378924f3dad694aa6b40fd5b21f7a8868
Matt Goerwell <Superninja246@hotmail.com>
2017-10-03 13:12:38 -0700

    Fixing activity switch

--- app/src/main/java/com/example/matt/a7051a3/MainActivity.java
+++ app/src/main/java/com/example/matt/a7051a3/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.matt.a7051a3;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void Search() {
+    public void Search(View view) {
         Intent intent = new Intent(this, SearchActivity.class);
         startActivity(intent);
     }
